b6aabf1d89d7ff40d059b15f7b0f0923f0a7cab2
Damien Georges damien.georges@ujf-grenoble.fr <damien.georges2@gmail.com>
2015-09-25 10:34:33 +0200

    update the script to add to our predictor table summer ndvi relative variables

--- scripts/0_reshape_mean_annual_ndvi_from_gee.R
+++ scripts/0_reshape_mean_annual_ndvi_from_gee.R
@@ -31,15 +31,68 @@
 
 
 ## intitialisation step --------------------------------------------------------
-
 rm(list = ls())
-setwd("~/Work/GODTHAABSFJORD/workdir/")
+# setwd("~/Work/GODTHAABSFJORD/workdir/") ## on pinea
+setwd("~/GOGTHAABSFJORD/workdir") ## on luke
+
+library(tidyr)
+library(dplyr)
 
 ## get the latest version of our annual mean summer ndvi  from modis
 # download.file("https://drive.google.com/open?id=0B9E3DXffjxAxa2xrYWRwc2xHZHc", 
 #               "godthaabsfjord_modis_ndvi5_mcd43a4.csv")
 
-## load the csv file
+## load the csv file containing data -------------------------------------------
 dat <- read.csv("godthaabsfjord_modis_ndvi5_mcd43a4.csv")
 head(dat)
+
+##' @note In this table we have the mean summer annual NDVI for each plot stored 
+##' under the name 'mean'
+
+
+## reshape the table -----------------------------------------------------------
+
+## 1. select only the info we need
+dat <- dat %>% select(plot, year, mean)
+
+## 2. reshape the table to have a column by period/year
+dat <- dat %>% spread(year, mean)
+
+## 3. update colnames to be more explicit
+colnames(dat)[grepl("^20[[:digit:]]+", colnames(dat))] <- paste0("mean_summ_ndvi_", grep("^20[[:digit:]]+", colnames(dat), value = TRUE))
+
+## 4. check that everithing is ok
+head(dat)
+
+## 5. save the table on hard drive
+write.csv(dat, file = "godthaabsfjord_modis_ndvi5_mcd43a4_reshaped.csv", row.names = FALSE)
+
+## 6. add to our predictor dataset
+dat.pred <- read.csv("../godthaabsfjordhub/data/godthaabsfjord_plots_fusion_table_with_pred.csv", stringsAsFactor = FALSE)
+dat.merg <- full_join(dat.pred, dat)
+
+## 7. check that everithing is ok
+head(dat.merg)
+
+## 8. add some extra computed indexs
+
+##    8.1 add the mean summer ndvi of the year of sampling
+dat.merg <- dat.merg %>% mutate(year = sub("-.*$", "", date))
+## TODO(DAMIEN) :  Do this with dplyr utilities
+dat.merg$mean_summ_ndvi_yos <- NA ## initialise the column; yos for year of sampling
+for(y in unique(dat.merg$year)){
+  dat.merg$mean_summ_ndvi_yos[ dat.merg$year == y ] <- dat.merg[dat.merg$year == y, paste0("mean_summ_ndvi_", y)]
+}
+head(dat.merg)
+
+##    8.2 add the cv of mean summer ndvi from 2001 to the year of observation
+## TODO(DAMIEN) :  Do this with dplyr utilities
+dat.merg$cv_mean_summ_ndvi_2001_to_yos <- NA ## initialise the column; yos for year of sampling
+for(y in unique(dat.merg$year)){
+  dat.merg$cv_mean_summ_ndvi_2001_to_yos[ dat.merg$year == y ] <- apply(dat.merg[dat.merg$year == y, paste0("mean_summ_ndvi_", 2001:y)], 1, raster::cv)
+}
+head(dat.merg)
+
+## 9. save the output
+write.csv(dat.merg, file = "../godthaabsfjordhub/data/godthaabsfjord_plots_fusion_table_with_pred.csv", row.names = FALSE)
 
